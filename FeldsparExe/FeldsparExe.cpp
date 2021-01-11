ab03661eee01dac63ab99fa37804821016a2208c
Zachary Meadows <zmeadows@gmail.com>
2021-01-10 16:43:22 -0800

    generate_moves in infinite loop for profiling

--- FeldsparExe/FeldsparExe.cpp
+++ FeldsparExe/FeldsparExe.cpp
@@ -1,4 +1,19 @@
-import bitboard;
-import rays;
+import game;
+import move;
+import movegen;
 
-int main() { return 0; }
+import<cstdio>;
+
+int main()
+{
+    const Game og = *Game::create("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+
+    MoveBuffer moves;
+    while (true) {
+        generate_moves<false, false>(og, moves);
+    }
+
+    printf("%llu\n", moves.length());
+
+    return 0;
+}
